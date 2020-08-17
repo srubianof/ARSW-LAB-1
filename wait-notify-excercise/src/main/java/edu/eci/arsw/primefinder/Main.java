bf53f7be20a329b6dcc2609f53979661484a6f06
Simon <german.marin@mail.escuelaing.edu.co>
2020-08-17 16:31:53 -0500

    wait notify

--- wait-notify-excercise/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ wait-notify-excercise/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -3,7 +3,7 @@ package edu.eci.arsw.primefinder;
 public class Main {
 
     public static void main(String[] args) {
-        Control control = Control.newControl(1000);
+        Control control = Control.newControl(5000);
         
         control.start();
 
